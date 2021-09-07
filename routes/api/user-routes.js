21dac604c6c1daf1b95e8419d5087d88cd506113
Katharine C Humble <katharinechumble@gmail.com>
2021-09-07 16:44:06 -0400

    updated password encryption

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -50,6 +50,28 @@ router.post('/', (req, res) => {
     });
 });
 
+router.post('/login', (req, res) => {
+
+    // Query operation
+    // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+  User.findOne({
+    where: {
+      email: req.body.email
+    }
+  }).then(dbUserData => {
+    if (!dbUserData) {
+      res.status(400).json({ message: 'No user with that email address!' });
+      return;
+    }
+
+    res.json({ user: dbUserData });
+
+    // Verify user
+
+  });  
+});
+  
+
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
       // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
